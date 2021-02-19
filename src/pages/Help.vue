4188917218c59657a3e075a672baad0fa83fcc5f
edias15 <emerson15dias@hotmail.com>
2021-02-19 07:45:43 -0300

    Color's Lists Management

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class='q-mt-none'>Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos voluptas laboriosam fuga quod. Aspernatur dignissimos quisquam commodi reiciendis tempora, nobis ad, molestias veniam laborum excepturi ab nisi voluptatem, enim eum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos voluptas laboriosam fuga quod. Aspernatur dignissimos quisquam commodi reiciendis tempora, nobis ad, molestias veniam laborum excepturi ab nisi voluptatem, enim eum.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Help'
+}
+</script>
